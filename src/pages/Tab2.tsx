5815875fb0eaf44e72eb03e8a86cd3946e207878
ntduncan <nateduncan0@.com>
2022-02-28 09:11:25 -0700

    added login and sign up forms

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,5 +1,5 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import LoginForm from '../components/Login/LoginForm';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
@@ -7,16 +7,11 @@ const Tab2: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Sign In</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <LoginForm />
       </IonContent>
     </IonPage>
   );
