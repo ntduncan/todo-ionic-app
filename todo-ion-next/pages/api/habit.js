d9450f84e4bb7c96b2f63b26c09e906960e8046b
ntduncan <nateduncan0@.com>
2022-03-03 22:21:17 -0700

    completed all habit routes except getting a single habit. I assume we are good to just get all

--- todo-ion-next/pages/api/habit.js
+++ todo-ion-next/pages/api/habit.js
@@ -2,25 +2,98 @@ import connectDB from '../../middleware/mongodb';
 import Habit from '../../models/habit';
 
 
-const handler = async (req, res) => {
+const handler = async (req, res, next) => {
+  /**
+   * CREATE HABIT ROUTE
+   */
     if (req.method === 'POST') {
-      // Check if name, email or password is provided
-      const { name, email, password } = req.body;
-      if (name && email && password) {
+      // Check if body has all the data provided
+      const { name, goal, isPositive, color, state } = req.body;
+      console.log(name, goal, isPositive, color, state)
+
+      if (name) {
           try {
-            // Hash password to store it in DB
-            var passwordhash = await bcrypt.sign(password);
-            var user = new User({
+            const habit = new Habit({
               name,
-              email,
-              password: passwordhash,
+              goal,
+              isPositive,
+              color,
+              state
             });
-            // Create new user
-            var usercreated = await user.save();
-            return res.status(200).send(usercreated);
+            // Add a new habit
+            const habitCreated = await habit.save();
+            return res.status(200).send(habitCreated);
           } catch (error) {
             return res.status(500).send(error.message);
           }
+
+        } else {
+          res.status(422).send('data_incomplete');
+        }
+
+    /**
+     * GET HABITS ROUTE
+     */
+    } else if (req.method === 'GET'){
+      Habit.find()
+      .then(habits => {
+        console.log(habits);
+        res.json(catches);
+      })
+      .catch(err => {
+        const error = new Error(err);
+        error.httpStatusCode = 500;
+        res.json(error);
+      });
+
+      /**
+      * DELETE ROUTE
+      */
+    } else if(req.method === "DELETE"){
+      const habitId = req.id;
+      console.log(habitId);
+      let deleteQuery = { _id: habitId };
+      Habit.findById(habitId).then((doc) => {
+         if (doc == null) {
+           console.log(habitId)
+            res.json({ message: "Habit was not found." });
+            return;
+         } else {
+            Habit.deleteOne(deleteQuery, function (err, obj) {
+               if (err) throw err;
+            });
+            res.json({ message: "Habit was deleted." });
+            return;
+         }
+      });}
+
+   /**
+   * Update HABIT ROUTE
+   */
+    else if (req.method === 'PATCH') {
+      // Check if body has all the data provided
+      const { id, name, goal, isPositive, color, state } = req.body;
+      console.log(id, name, goal, isPositive, color, state)
+
+      if (name) {
+          try {
+            const habit = {
+              name,
+              goal,
+              isPositive,
+              color,
+              state
+            }
+            await Habit.findByIdAndUpdate(id, habit)
+            .then(() => res.status(200).send("updated habit"))
+            .catch((err) => {
+               console.log(err);
+               res.send(err.statusCode, err.message);
+            });
+          } catch (error) {
+            return res.status(500).send(error.message);
+          }
+
         } else {
           res.status(422).send('data_incomplete');
         }
